4a6897aa5d590e6614a16aced54c453049d082b0
William Sankey <WSankey@gmail.com>
2015-08-14 18:55:48 -0400

    Updated templates.

--- sitebuilder.py
+++ sitebuilder.py
@@ -1,5 +1,8 @@
+import sys
+
 from flask import Flask, render_template
 from flask_flatpages import FlatPages
+from flask_frozen import Freezer
 
 DEBUG = True
 FLATPAGES_AUTO_RELOAD = DEBUG
@@ -8,19 +11,24 @@ FLATPAGES_EXTENSION = '.md'
 app = Flask(__name__)
 app.config.from_object(__name__)
 pages = FlatPages(app)
+freezer = Freezer(app)
 
 @app.route("/")
 def index():
 	return render_template('index.html', pages=pages)
 
-@app.route('/<path:path>/')
-def page(path):
-    return pages.get_or_404(path).html
-
 @app.route('/tag/<string:tag>/')
 def tag(tag):
 	tagged = [p for p in pages if tag in p.meta.get('tags', [])]
 	return render_template('tag.html', pages=tagged, tag=tag)
+
+@app.route('/<path:path>')
+def page(path):
+	page = pages.get_or_404(path)
+	return render_template('page.html', page=page)
 	
 if __name__ == "__main__":
-    app.run(port=8000)
+	if len(sys.argv) > 1 and sys.argv[1] == "build":
+		freezer.freeze()
+	else:
+		app.run(port=8000)
