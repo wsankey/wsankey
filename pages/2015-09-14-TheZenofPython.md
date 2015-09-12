1108161725d00bf5ff285759931f049dfa7893a0
William Sankey <WSankey@gmail.com>
2015-09-12 12:14:41 -0400

    Added redirection

---
+++ pages/2015-09-14-TheZenofPython.md
@@ -0,0 +1,11 @@
+title: The Zen of Python
+date: 2015-09-14
+categories: [programming]
+tags: [FivePy]
+podcast: FivePy
+description: Episode 001 of FivePy
+
+We start the first episode of FivePy with the Zen of Python, which can be retrieved from the REPL simply:
+<pre><code class="language-python">
+import this
+</code></pre>
--- sitebuilder.py
+++ sitebuilder.py
@@ -1,6 +1,6 @@
 import sys
 
-from flask import Flask, render_template, render_template_string, Markup
+from flask import Flask, render_template, render_template_string, Markup, redirect, url_for
 from flask_flatpages import FlatPages, pygmented_markdown
 from flask_frozen import Freezer
 
@@ -34,17 +34,28 @@ def projects():
 
 @app.route("/posts/")
 def blog():
-	return render_template('blog.html', pages=pages)
+	articles = (p for p in pages if "date" in p.meta)
+	latest = sorted(articles, reverse=True, 
+							key=lambda p: p.meta['date'])
+	return render_template('blog.html', pages=latest)
 
 @app.route('/tag/<string:tag>/')
 def tag(tag):
-	tagged = [p for p in pages if tag in p.meta.get('tags', [])]
-	return render_template('tag.html', pages=tagged, tag=tag)
+	if tag == "FivePy":
+		return redirect(url_for('fivepy'))
+	else:
+		tagged = [p for p in pages if tag in p.meta.get('tags', [])]
+		return render_template('tag.html', pages=tagged, tag=tag)
 
 @app.route('/posts/<path:path>/')
 def page(path):
 	page = pages.get_or_404(path)
 	return render_template('page.html', page=page)
-	
+
+@app.route('/fivepy/')
+def fivepy():
+	posts = [p for p in pages if p.meta['tags'] == ['FivePy']]
+	return render_template('fivepy.html', pages=posts)
+
 if __name__ == "__main__":
 	app.run(port=8000)
